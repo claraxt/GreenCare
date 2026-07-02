ba2e3414d56b3e97e4140192ac3c1b0a94ae443f
Patricia <boldt1304@gmail.com>
2026-07-02 19:53:46 +0200

    struktur geändert

--- src/app/pages/map/map.page.ts
+++ src/app/pages/map/map.page.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import * as L from 'leaflet';
+
 
 @Component({
   selector: 'app-map',
@@ -9,12 +11,56 @@ import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/stan
   styleUrls: ['./map.page.scss'],
   standalone: true,
   imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+
 })
-export class MapPage implements OnInit {
+export class MapPage implements OnInit, AfterViewInit {
 
   constructor() { }
+  map: any;
+  marker: L.Marker | null = null;
+  circle: L.Circle | null = null;
+
 
   ngOnInit() {
+
+  }
+  ngAfterViewInit() {
+    navigator.geolocation.getCurrentPosition(
+      (pos) => this.showMap(pos),
+      (err) => alert('GPS nicht erlaubt')
+    );
   }
 
+  showMap(pos: GeolocationPosition) {
+    const lat = pos.coords.latitude;
+    const lng = pos.coords.longitude;
+
+    this.map = L.map('map').setView([lat, lng], 16);
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; OpenStreetMap contributors'
+    }).addTo(this.map);
+
+    this.marker = L.marker([lat, lng]).addTo(this.map);
+    this.circle = L.circle([lat, lng], {
+      radius: pos.coords.accuracy
+    }).addTo(this.map);
+
+    navigator.geolocation.watchPosition(
+      (p) => this.updatePosition(p),
+      (e) => console.log(e)
+    );
+  }
+  updatePosition(pos: GeolocationPosition) {
+    const lat = pos.coords.latitude;
+    const lng = pos.coords.longitude;
+
+    this.marker?.setLatLng([lat, lng]);
+    this.circle?.setLatLng([lat, lng]);
+    this.circle?.setRadius(pos.coords.accuracy);
+
+    this.map.panTo([lat, lng]);
+  }
 }
+
+

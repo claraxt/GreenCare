bc66b675c40deac09b2ca5b20735ea284fe48d83
claraxt <tiemannclara521@gmail.com>
2026-07-17 12:38:52 +0200

    Fotos eingefügt

--- src/app/pages/explore/explore.page.ts
+++ src/app/pages/explore/explore.page.ts
@@ -1,7 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonLabel, IonSegment, IonSegmentButton, IonCard, IonCardContent, IonIcon  } from '@ionic/angular/standalone';
+
+import {
+  IonContent,
+  IonHeader,
+  IonTitle,
+  IonToolbar,
+  IonLabel,
+  IonSegment,
+  IonSegmentButton,
+  IonCard,
+  IonCardContent,
+  IonIcon
+} from '@ionic/angular/standalone';
+
 import { addIcons } from 'ionicons';
 import { person } from 'ionicons/icons';
 
@@ -10,46 +23,163 @@ import { person } from 'ionicons/icons';
   templateUrl: './explore.page.html',
   styleUrls: ['./explore.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, IonLabel, IonSegment, IonSegmentButton, IonCard, IonCardContent,IonIcon, CommonModule, FormsModule]
+  imports: [
+    IonContent,
+    IonHeader,
+    IonTitle,
+    IonToolbar,
+    IonLabel,
+    IonSegment,
+    IonSegmentButton,
+    IonCard,
+    IonCardContent,
+    IonIcon,
+    CommonModule,
+    FormsModule
+  ]
 })
+
 export class ExplorePage implements OnInit {
 
-  constructor() { 
-     addIcons({
-    person
-  });
+  constructor() {
+    addIcons({
+      person
+    });
   }
 
-  ngOnInit() {
-  }
+  selectedSegment = 'vorgeschlagen';
 
-   selectedSegment = 'vorgeschlagen';
+  plants: any[] = [];
 
-  plants = [
+  plantsSuggested = [
 
     {
       name: 'Park am See',
-      description: 'Blumenbeete und Wiese am See',
-      distance: '0,8 km',
-      peopleNeeded: 5,
-      task: 'Gießen und Rasen mähen'
-    },
-
-    {
-      name: 'Friedhof',
-      description: 'Grab der Familie Müller',
-      distance: '1,2 km',
-      peopleNeeded: 2,
-      task: 'Gießen und Unkraut jäten'
-    },
-
-    {
-      name: 'Stadtplatz',
       description: 'Blumenbeet am Weg',
-      distance: '2,3 km',
+      distance: '800 m',
+      peopleNeeded: 2,
+      task: 'Gießen',
+      image: 'assets/Fotos/park.jpeg'    
+    },
+
+
+    {
+      name: 'Friedhof Nord',
+      description: 'Rosenbeet',
+      distance: '1,2 km',
       peopleNeeded: 3,
-      task: 'Gießen'
-    }
+      task: 'Unkraut entfernen, Gießen, schneiden',
+      image: 'assets/Fotos/friedhof.jpeg'
+    },
+
+     {
+      name: 'Stadtpark',
+      description: 'Parkbeete',
+      distance: '1,9 km',
+      peopleNeeded: 6,
+      task: 'Unkraut entfernen, Gießen',
+      image: 'assets/Fotos/stadtpark.jpeg'
+    },
+
+    {
+      name: 'Freibad',
+      description: 'Blumen an der Rasenfläche',
+      distance: '2,0 m',
+      peopleNeeded: 2,
+      task: 'Unkrauft entfernen',
+      image: 'assets/Fotos/freibad.jpeg'
+    },
 
   ];
+
+  plantsNearby = [
+
+    {
+      name: 'Marktplatz',
+      description: 'Blumenkübel',
+      distance: '200 m',
+      peopleNeeded: 1,
+      task: 'Bewässern',
+      image: 'assets/Fotos/marktplatz.jpeg' 
+    },
+
+    {
+      name: 'Schulhof',
+      description: 'Wildblumenbeet',
+      distance: '450 m',
+      peopleNeeded: 2,
+      task: 'Gießen, Unkrauft entfernen',
+      image: 'assets/Fotos/schulhof.jpeg'
+    },
+
+    {
+      name: 'Krankenhaus',
+      description: 'Blumenbeet am Eingang',
+      distance: '500 m',
+      peopleNeeded: 1,
+      task: 'Gießen',
+      image: 'assets/Fotos/krankenhaus.jpeg'
+    },
+
+  ];
+
+  plantsNew = [
+
+    {
+      name: 'Bahnhof',
+      description: 'Blumen am Eingang',
+      distance: '1,9 km',
+      peopleNeeded: 1,
+      task: 'gießen',
+      image: 'assets/Fotos/bahnhof.jpeg'
+    },
+
+    {
+      name: 'Kirchplatz',
+      description: 'mehrere Beete an dem Platz',
+      distance: '2,1 km',
+      peopleNeeded: 4,
+      task: 'Pflege der Beete',
+      image: 'assets/Fotos/kirchplatz.jpeg'
+    },
+
+  ];
+
+  ngOnInit() {
+
+    this.plants = this.plantsSuggested;
+
+  }
+
+  segmentChanged(event: any) {
+
+    this.selectedSegment = event.detail.value;
+
+    if (this.selectedSegment === 'vorgeschlagen') {
+
+      this.plants = this.plantsSuggested;
+
+    }
+
+    if (this.selectedSegment === 'naehe') {
+
+      this.plants = this.plantsNearby;
+
+    }
+
+    if (this.selectedSegment === 'neu') {
+
+      this.plants = this.plantsNew;
+
+    }
+
+  }
+
+  openPlant(plant: any) {
+
+    console.log(plant);
+
+
+  }
+
 }

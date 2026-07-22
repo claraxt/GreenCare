bd98d6b5f899bd4d5215e1eeda7cba25d5655d1d
claraxt <tiemannclara521@gmail.com>
2026-07-22 13:07:48 +0200

    Community Page Grundstruktur gelegt

--- src/app/pages/community/community.page.ts
+++ src/app/pages/community/community.page.ts
@@ -1,9 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonCard, IonCardContent } from '@ionic/angular/standalone';
-import { Plant } from 'src/app/models/plant';
-import { PlantService } from 'src/app/services/plant.service';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonSegment, IonSegmentButton, IonLabel, IonCard, IonCardContent, IonButton } from '@ionic/angular/standalone';
 
 
 @Component({
@@ -11,38 +9,75 @@ import { PlantService } from 'src/app/services/plant.service';
   templateUrl: './community.page.html',
   styleUrls: ['./community.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonCard, IonCardContent]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, IonSegment, IonSegmentButton, IonLabel, IonCard, IonCardContent, IonButton, CommonModule, FormsModule]
 })
 export class CommunityPage implements OnInit {
 
-  plants: Plant[] = [];
 
-  constructor(private plantService: PlantService) {
+    selectedSegment = "fragen";
+    posts: any[] = [];
+    questionPosts = [
+      {
+        id: 1,
+        user:"Lisa",
+        image:"assets/Fotos/rosenwerdengelb.jpeg",
+        title:"Warum werden meine Rosen gelb?",
+        answers:4
+      },
+
+      {
+        id:2,
+        user:"Tim",
+        image:"assets/Fotos/krankerlavendel.jpeg",
+        title:"Was fehlt meinem Lavendel?",
+        answers:2
+      }
+    ];
+
+    tipPosts = [
+      {
+        id:1,
+        user:"Anna",
+        image:"assets/Fotos/friedhof.jpeg",
+        title:"Lavendel lieber selten, aber gründlich gießen.",
+        likes:16
+      },
+
+      {
+        id:2,
+        user:"Max",
+        image:"assets/Fotos/freibad.jpeg",
+        title:"Unkraut am besten nach Regen entfernen.",
+        likes:11
+      }
+    ];
+  
+    constructor() { }
+
+  ngOnInit() {
+
+    this.posts = this.questionPosts;
 
   }
 
-  async ngOnInit() {
+  segmentChanged(event: any) {
+    this.selectedSegment = event.detail.value;
+    if (this.selectedSegment === 'fragen') {
+      this.posts = this.questionPosts;
+    }
 
-    console.log("Community geladen");
+    if (this.selectedSegment === 'tipps') {
+      this.posts = this.tipPosts;
+    }
 
-    const result = await this.plantService.plantInfo();
-
-    console.log("API Ergebnis:", result);
-
-    this.plants = result;
+    if (this.selectedSegment === 'lexikon') {
+      this.posts = [];
+    }
 
   }
 
-  /*plants: Plant[] = [];
-
-  constructor(private trefle: TrefleService) { }
-
-  search(value: string) {
-
-    this.trefle.searchPlants(value).subscribe(result => {
-      this.plants = result.data;
-    });
-
-  }*/
+  newPost() {
+    console.log("Neue Frage");
+  }
 
 }

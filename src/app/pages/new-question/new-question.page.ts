b0437d78c1687d2a02af2526faf3119aa321d8ab
claraxt <tiemannclara521@gmail.com>
2026-07-24 19:15:58 +0200

    Community

--- src/app/pages/new-question/new-question.page.ts
+++ src/app/pages/new-question/new-question.page.ts
@@ -1,14 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonInput, IonTextarea, IonButton, IonBackButton } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonInput, IonTextarea, IonButton, IonButtons, IonBackButton, IonItem } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-new-question',
   templateUrl: './new-question.page.html',
   styleUrls: ['./new-question.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, IonInput, IonTextarea, IonButton, IonBackButton, CommonModule, FormsModule]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, IonInput, IonTextarea, IonButton, IonButtons, IonBackButton, IonItem, CommonModule, FormsModule]
 })
 export class NewQuestionPage implements OnInit {
 

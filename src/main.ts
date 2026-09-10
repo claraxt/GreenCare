c33e5911f7a100ff1579d987bf95d97f077409c6
Patricia <boldt1304@gmail.com>
2026-09-10 20:31:40 +0200

    icon

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,6 @@ import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } 
 import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
-//import { provideHttpClient } from '@angular/common/http';
 import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
 import { provideFirestore, getFirestore } from '@angular/fire/firestore';
 import { environment } from './environments/environment';
